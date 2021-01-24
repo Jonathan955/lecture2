50675fab04f95812c2742e3fcdd508902e393845
Jonathan955 <Jonathan_955@users.noreply.github.com>
2021-01-24 23:29:09 +0100

    Assignment
    Example 3

--- example3/script.js
+++ example3/script.js
@@ -38,7 +38,7 @@ function init() {
     const loader = new Rhino3dmLoader()
     loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@0.13.0/' )
 
-    loader.load( 'sphere.3dm', function ( object ) {
+    loader.load( 'Example 3.3dm', function ( object ) {
 
         document.getElementById('loader').remove()
         scene.add( object )
@@ -69,7 +69,7 @@ function onClick( event ) {
     // reset object colours
     scene.traverse((child, i) => {
         if (child.isMesh) {
-            child.material.color.set( 'white' )
+            child.material.color.set( 'Black' )
         }
     });
 
@@ -79,7 +79,7 @@ function onClick( event ) {
         const object = intersects[0].object
         console.log(object) // debug
 
-        object.material.color.set( 'yellow' )
+        object.material.color.set( 'pink' )
 
         // get user strings
         let data, count
